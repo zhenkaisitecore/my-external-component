4fbafa9ea8dfdb18ded2b00194953e803e4dd855
zhe <zhenkai.wong@sitecore.com>
2023-12-08 18:20:53 +0800

    Updated index.tsx

--- app/byoc/index.tsx
+++ app/byoc/index.tsx
@@ -1,14 +1,6 @@
-// import * as FEAAS from "@sitecore-feaas/clientside/react";
-// import ClientsideBundle from "./index.client";
-// import dynamic from "next/dynamic";
-
-// FEAAS.enableNextClientsideComponents(dynamic, ClientsideBundle);
-
-"use client";
 import * as FEAAS from "@sitecore-feaas/clientside/react";
-export default (props: any) => {
-  debugger;
-  return FEAAS.ExternalComponent(props);
-};
-import "./Hello"
+import ClientsideBundle from "./index.client";
+import dynamic from "next/dynamic";
+
+FEAAS.enableNextClientsideComponents(dynamic, ClientsideBundle);
 
